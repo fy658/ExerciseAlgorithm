5aaefbb288836fe40f179ee2f6d0005038fdd1b7
Fang Yu <fy658uow@163.com>
2024-09-27 15:39:03 +0800

    链表区间翻转

--- LinkedList.py
+++ LinkedList.py
@@ -38,6 +38,56 @@ while cur_node:
     cur_node = cur_node.next
 
 
+## 链表在指定区间内翻转
+
+class Solution2:
+    def ReverseBetweenInterval(self, head: ListNode, m: int, n: int):
+        if not head:
+            return None
+        dummy_head = ListNode(-1)
+        dummy_head.next = head
+        prev = dummy_head
+        curr = head
+
+        for _ in range(1, m):
+            prev = curr
+            curr = curr.next
+
+        for _ in range(m, n):
+            temp_var = curr.next
+            curr.next = temp_var.next
+            temp_var.next = prev.next
+            prev.next = temp_var
+        return dummy_head.next
+
+
+
+node_list1 = ListNode(1)
+node_list2 = ListNode(2)
+node_list3 = ListNode(3)
+node_list4 = ListNode(4)
+node_list5 = ListNode(5)
+node_list1.next = node_list2
+node_list2.next = node_list3
+node_list3.next = node_list4
+node_list4.next = node_list5
+
+current_node = node_list1
+print("\nbefore reverse ********")
+while current_node:
+    print(current_node.val, end=" ")
+    current_node = current_node.next
+
+solution2 = Solution2().ReverseBetweenInterval(node_list1,2, 4)
+curr_node = node_list1
+print("\n")
+print("after reverse *********：")
+while curr_node:
+    print(curr_node.val, end=" ")
+    curr_node = curr_node.next
+
+
+
 
 
 
